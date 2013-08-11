ade66e2e267cf2fd8ce2dc9641f045598777e46d
soyeoneone <xinersl@126.com>
2013-08-11 10:05:38 +0800

    restore first with rspec test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DorisBlog::Application.config.secret_key_base = '7c39d7474cfc7e1472649eca1dd4a067475c552569b6d9d9df9cf2014ce13a4d24a6cb6fa159bbdaaa13a2232b18da27f2ca851cd3cd09aeb32ef7576799496b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DorisBlog::Application.config.session_store :cookie_store, key: '_DorisBlog_session'
